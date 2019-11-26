c4f6f4bb7254aa0c03cd0247f2b53a8ab9642b5d
Robert Krahn <robert.krahn@gmail.com>
2019-11-26 18:09:18 +0100

    average db values

--- src/fritz_homeautomation/db_access.clj
+++ src/fritz_homeautomation/db_access.clj
@@ -12,30 +12,30 @@
            org.joda.time.Duration))
 
 
-(defn no-underscore [string]
+(defn- no-underscore [string]
   (string/replace string #"_" "-"))
 
-(defn query
-  ([table]
-   (query table nil nil))
-  ([table start-time]
-   (query table start-time nil))
-  ([table start-time end-time]
-   (let [start (when start-time (format "time >= timestamp '%s'" start-time))
-         end (when end-time (format "time <= timestamp '%s'" end-time))
-         where (some->> [start end] (keep not-empty) seq (string/join " AND "))
-         query (str "select * from " (name table))
-         query (if where (str query " where " where) query)
-         query (str query " order by device_id, time")]
-     query)))
+(defn- query
+  ([table attr time-group]
+   (query table attr time-group nil nil))
+  ([table attr time-group start-time]
+   (query table attr time-group start-time nil))
+  ([table attr time-group start-time end-time]
+   (str "
+SELECT device_id, " attr ", time_group AS time
+FROM (SELECT device_id AS device_id, avg(" attr ") AS " attr ", date_trunc('" time-group "', time) AS time_group
+      FROM " table "
+      WHERE time >= timestamp '" start-time "'" (if end-time (str " AND time <= timestamp '" end-time "'") "") "
+      GROUP BY device_id, time_group
+      ORDER BY device_id, time_group) AS data;")))
 
 (defn query! [start-time end-time]
   (let [watts (->>
-               (db/with-connection (db/execute! [(query "watts" start-time end-time)]))
+               (db/with-connection (db/execute! [(query "watts" "watt" "hour" start-time end-time)]))
                (transform [ALL MAP-KEYS] #(-> % name no-underscore keyword)))
         watts-by-device (group-by :device-id watts)
         temps (->>
-               (db/with-connection (db/execute! [(query "temperature" start-time end-time)]))
+               (db/with-connection (db/execute! [(query "temperature" "temp" "hour" start-time end-time)]))
                (transform [ALL MAP-KEYS] #(-> % name no-underscore keyword)))
         temps-by-device (group-by :device-id temps)]
     {:temps temps-by-device
@@ -65,7 +65,72 @@
 
 (comment
 
+  (def x (query! month-start month-end))
+  (query :temperature month-start month-end)
+
+  "select * from temperature where time >= timestamp '2019-11-01T00:00' AND time <= timestamp '2019-11-30T23:59' order by device_id, time"
+  (-> x :temps vals second count)
+
+  (->>
+   (db/with-connection (db/execute! [(query :temperature day-start)]))
+   (transform [ALL MAP-KEYS] #(-> % name no-underscore keyword)))
+
+  (let [
+        table "temperature"
+        attr "temp"
+        ;; table "watts"
+        ;; attr "watt"
+        time-group "hour"
+        start-time day-start
+        end-time day-end] (->>
+           (db/with-connection (db/execute! [(str "
+SELECT device_id, " attr ", time_group AS time
+FROM (SELECT device_id AS device_id, avg(" attr ") AS " attr ", date_trunc('" time-group "', time) AS time_group
+      FROM " table "
+      WHERE time >= timestamp '" start-time "' AND time <= timestamp '" end-time "'
+      GROUP BY device_id, time_group
+      ORDER BY device_id, time_group) AS data;")]))
+           (transform [ALL MAP-KEYS] #(-> % name no-underscore keyword))
+           clojure.pprint/pprint))
+
+  (query! day-start day-end)
+
+  (clojure.pprint/pprint (db/with-connection (db/execute! ["SELECT device_id as device_id, temp, to_timestamp(x, 'YYYYMMDD HH24') as time FROM (SELECT device_id as device_id, avg(temp) as temp, to_char(time, 'YYYYMMDD HH24') as x FROM temperature group by device_id, x order by x) as temp limit 5;"])))
+  (clojure.pprint/pprint (db/with-connection (db/execute! ["SELECT count(*) FROM (SELECT avg(temp) as temp, to_char(time, 'YYYYMMDD HH24') as x FROM temperature group by x order by x) as temp;"])))
+  (clojure.pprint/pprint (db/with-connection (db/execute! ["SELECT watt, to_timestamp(x, 'YYYYMMDD HH24') as time FROM (SELECT avg(watt) as watt, to_char(time, 'YYYYMMDD HH24') as x FROM watts group by x order by x) as watts;"])))
+  (clojure.pprint/pprint (db/with-connection (db/execute! ["SELECT count(*) FROM (SELECT avg(watt) as watt, to_char(time, 'YYYYMMDD HH24') as x FROM watts group by x order by x) as watts;"])))
+
+  (db/with-connection (db/execute! ["SELECT t
+FROM   generate_series('2004-03-07'::timestamp
+                     , '2004-03-08'::timestamp
+                     ,  '10 minutes'::interval) AS t;"]))
+
   (def x (query-some!))
+
+  (def one-minute (* 1000 60))
+  (def one-hour (* 1000 60 60))
+
+  (loop [data (-> x :this-month :temps (get "11630 0069103"))
+         fixed-data []
+         result []]
+    (let [[a b & _] data]
+      (if (or (not a) (not b))
+        fixed-data
+        (let [dt (- (.getTime (:time b)) (.getTime (:time a)))
+              fixed-data (if (> dt one-hour)
+                           (conj fixed-data
+                                 a
+                                 (assoc a
+                                        :time (java.sql.Timestamp. (+ (.getTime (:time a)) one-minute))
+                                        :temp 0)
+                                 (assoc b
+                                        :time (java.sql.Timestamp. (- (.getTime (:time b)) one-minute))
+                                        :temp 0)
+                                 b)
+                           (conj fixed-data a b))]
+          (recur (rest data)
+                 (conj fixed-data)
+                 (conj result dt))))))
 
   (require 'clojure.contrib.humanize)
   (clojure.contrib.humanize/filesize (count (pr-str x)))
